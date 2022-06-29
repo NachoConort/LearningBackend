b2093bf7a258efcb02e2239b8728805c102203f2
NachoConort <nachoconort@gmail.com>
2022-06-28 21:10:06 -0300

    Installed dotenv

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const port = process.env.PORT;
+
 
 app.get('/', (req, res) => {
-  res.send('Funcionaaaa')
+  res.send('Funcionaaaa, funciona mejor :)')
 });
 
-app.listen(4001, () => {
-  console.log('Listening in port 4001')
+app.listen(port, () => {
+  console.log(`Listening in port ${port}`)
 });
